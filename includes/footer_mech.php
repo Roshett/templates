83f268e26ecd6947ae211e45e1e4f97e3ca41d50
roshett <roshette@yandex.ru>
2017-07-18 15:20:49 +0300

    someFix

--- includes/footer_mech.php
+++ includes/footer_mech.php
@@ -1,3 +1,10 @@
+<!--[if lt IE 9]>
+	<script src="libs/html5shiv/es5-shim.min.js"></script>
+	<script src="libs/html5shiv/html5shiv.min.js"></script>
+	<script src="libs/html5shiv/html5shiv-printshiv.min.js"></script>
+	<script src="libs/respond/respond.min.js"></script>
+<![endif]-->
+
 <script src="https://code.jquery.com/jquery-3.1.1.slim.min.js" integrity="sha384-A7FZj7v+d/sdmMqp/nOQwliLvUsJfDHW+k9Omg/a/EheAdgtzNs3hpfag6Ed950n" crossorigin="anonymous"></script>
 <script src="js/main.js"></script>
 </body>
--- includes/header.php
+++ includes/header.php
@@ -1,9 +1,21 @@
 <!-- Start template by Roshette -->
 <!DOCTYPE html>
-<html lang="en">	
+<html lang="ru">	
 <head>
+
 	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+	<meta name="description" content="">
+
 	<title>Template</title>
+	
+	<!-- favicon -->
+	<link rel="shortcut icon" href="img/favicon/favicon.ico" type="image/x-icon">
+	<link rel="apple-touch-icon" href="img/favicon/apple-touch-icon.png">
+	<link rel="apple-touch-icon" sizes="72x72" href="img/favicon/apple-touch-icon-72x72.png">
+	<link rel="apple-touch-icon" sizes="114x114" href="img/favicon/apple-touch-icon-114x114.png">
+
 	<!-- fonts -->
 	<link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,700,700i|Roboto:300,300i,400&amp;subset=cyrillic,latin-ext" rel="stylesheet"> 
 	<!-- libs -->
